4ed590b3b1fe1f331403080cd6ffdf607cefd8af
Alexander Shvetsov <ash@fanky.dev>
2023-06-21 11:50:51 +0500

    🔨 render a part of date from API

--- js/modules/widgetService.js
+++ js/modules/widgetService.js
@@ -1,11 +1,19 @@
-import { renderWidgetForecast, renderWidgetOther, renderWidgetToday } from './widgetRender.js';
+import { fetchWeather } from './APIservice.js';
+import { renderWidgetForecast, renderWidgetOther, renderWidgetToday, showError } from './widgetRender.js';
 
-export const startWidget = () => {
+export const startWidget = async () => {
   const widget = document.createElement('div');
   widget.classList.add('widget');
 
-  renderWidgetToday(widget);
-  renderWidgetOther(widget);
+  const weatherData = await fetchWeather('Екатеринбург');
+
+  if (weatherData.success) {
+    renderWidgetToday(widget, weatherData?.data);
+    renderWidgetOther(widget, weatherData?.data);
+  } else {
+    showError(widget, 'Error!');
+  }
+
   renderWidgetForecast(widget);
 
   return widget;
